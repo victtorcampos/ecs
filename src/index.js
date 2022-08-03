8e3af516b6289844badaa63fbcdf24baf68ed04b
Victor Campos <victor-campos@outlook.com>
2022-08-03 17:31:18 -0400

    inicio projeto

--- src/index.js
+++ src/index.js
@@ -33,7 +33,6 @@ root.render(
 function logger({ getState }) {
   return next => action => {
     console.log('will dispatch', action);
-
     const returnValue = next(action);
     console.log('state after dispatch', getState())
     return returnValue
